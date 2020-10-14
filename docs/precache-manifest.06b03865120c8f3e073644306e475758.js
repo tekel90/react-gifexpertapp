107f7048f7978c1322ecb2a20bf71bd19ca7f48e
Jonathan Tekel <tekel.jonathan@gmail.com>
2020-10-14 01:41:54 -0300

    segundo commit

---
+++ docs/precache-manifest.06b03865120c8f3e073644306e475758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f638e2478f66e17d49dced7e01bdf43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0dc58a004fe10c3b56a",
+    "url": "/static/css/main.536112a4.chunk.css"
+  },
+  {
+    "revision": "ccd9761b7db298367323",
+    "url": "/static/js/2.dab6626b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab6626b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0dc58a004fe10c3b56a",
+    "url": "/static/js/main.49bef5ba.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
